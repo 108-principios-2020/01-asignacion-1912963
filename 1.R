7b57f6b23ecc7fd719fe8bc29ba75eacf3118101
Adriana Garza <adriana.c_gza@hotmail.com>
2020-02-06 13:49:38 -0600

    Actualizar

--- 1.R
+++ 1.R
@@ -8,16 +8,11 @@ conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?d
 mean(conjunto$Altura)
 mean(conjunto$Diametro)
 mean(conjunto$Vecinos)
-mean(H.media$Altura)
-mean(H.16$Altura)
-mean(conjunto$vecinos)
-mean()
-mean(vecinos_3$Vecinos)
-mean(vecinos_4$Vecinos)
 
 
-H.media <- subset(conjunto, conjunto$Altura <= 13.9432)
-H.16 <- subset(conjunto,conjunto$Altura < 16.5)
+
+
+
 ts.douglasia <- conjunto$Especie == "H"| conjunto$Especie =="F"
 
 # altura  -----------------------------------------------------------------
@@ -25,13 +20,20 @@ ts.douglasia <- conjunto$Especie == "H"| conjunto$Especie =="F"
 
 H.media <-subset(conjunto, conjunto$Altura <= 13.9432)
 
+mean(H.media$Altura)
+
+
 H.16 <- subset(conjunto,conjunto$Altura < 16.5)
+
+mean(H.16$Altura)
 
 
 # Vecinos -----------------------------------------------------------------
 
 vecinos_3 <- subset(conjunto,conjunto$Vecinos <= 3) 
 vecinos_4 <- subset(conjunto,conjunto$Vecinos > 4)
+mean(vecinos_3$Vecinos)
+mean(vecinos_4$Vecinos)
 
 
 # Diámetro ----------------------------------------------------------------
@@ -39,6 +41,8 @@ vecinos_4 <- subset(conjunto,conjunto$Vecinos > 4)
 DBH_media <- subset(conjunto, conjunto$Diametro < 15.794)
 DBH_16 <- subset(conjunto, conjunto$Diametro > 16)
 
+mean(DBH_16$Diametro)
+mean(DBH_media$Diametro)
 
 # Especie -----------------------------------------------------------------
 
@@ -47,36 +51,36 @@ Diametrocedrorojo <- subset(Cedro_rojo, Cedro_rojo$Diametro <= 16.9)
 Alturacedrorojo <- subset(Cedro_rojo, Cedro_rojo$Altura > 18.5)
 
 Tsuga <- subset(conjunto, conjunto$Especie == "H")
-Diametrotsuga <- subset(Tsuga, Tsuga$Diametro <= 16.9)
-Alturatsuga <- subset(Tsuga, Tsuga$Altura > 18.5)
-
 Douglasia <- subset(conjunto, conjunto$Especie == "F")
-Diametrodou <- subset(Douglasia, Douglasia$Diametro <= 16.9)
-Alturadou <- subset(Douglasia, Douglasia$Altura >= 18.5)
-
-
+ts.douglasia <- subset(conjunto, conjunto$Especie == "H"| conjunto$Especie =="F")
 
 # Histogramas -------------------------------------------------------------
 
-hist(conjunto$Altura, col="yellow",xlab ="Altura", ylab = "frecuencia", main ="Histograma de altura")
-hist(conjunto$Diametro, col="red",xlab ="Diametro", ylab = "frecuencia", main ="Histograma de Diametro")
-hist(conjunto$Vecinos, col="orange",xlab ="Vecinos", ylab = "frecuencia", main ="Histograma de Vecinos")
+par(mfrow=c(2,2))
+hist(Tsuga$Altura, main = "Altura especie H", 
+     col="red", xlab="Alturas", ylab="Frecuencia")
+hist(Douglasia$Altura, main= "Altura especie F",
+     col="blue", xlab="Alturas", ylab="Frecuencia")
+hist(Tsuga$Diametro , main = "Diametro especie H",
+     col="yellow", xlab="Altura", ylab="Frecuencia")
+hist(Douglasia$Diametro, main= "Diametro especie F",
+     col="pink", xlab="Altura", ylab="Frecuencia")
+par(mfrow=c(1,1))
 
 
 
 # desviaciones estandar ---------------------------------------------------
 
 sd(conjunto$Altura)
+sd(H.media$Altura)
+sd(H.16$Altura)
+
+sd(DBH_16$Diametro)
+sd(DBH_media$Diametro)
 sd(conjunto$Diametro)
+
 sd(conjunto$Vecinos)
+sd(vecinos_3$Vecinos)
+sd(vecinos_4$Vecinos)
 
 
-# gráficas ----------------------------------------------------------------
-
-par(mfrow=c(2,2))
-hist(Tsuga$Altura, main = "Altura especie H", 
-     col="red", xlab="frecuencia")
-hist(Douglasia$Altura, main= "Altura especie F")
-hist(Tsuga$Diametro, main = "Diametro especie H")
-hist(Douglasia$Diametro, main = "Diametro especie F")
-par(mfrow=c(1,1))
